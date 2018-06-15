bf20194a1b4d4b1c749b6e88402bc993a965f231
Trey Decker <treydecker@Treys-MacBook-Air.local>
2018-06-14 22:59:11 -0600

    scrollIntoView slightly working

--- app/app.js
+++ app/app.js
@@ -1,11 +1,25 @@
-document.addEventListener('DOMContentLoaded', () => {
-  let elems = document.querySelectorAll('.parallax'),
-  instances = M.Parallax.init(elems, {});
+// document.addEventListener('DOMContentLoaded', () => {
+//   let elems = document.querySelectorAll('.parallax'),
+//   instances = M.Parallax.init(elems, {}),
+//   loader = document.getElementById('loader');
 
-  document.querySelectorAll('a[href="#about"]')[0].addEventListener('click', (event) => {
-    event.preventDefault();
-    document.querySelector('#about').scrollIntoView({
-      behavior: 'smooth'
-    });
-  });
-})
+//   loader.classList = "shrink";
+//   document.querySelectorAll('#content')[0].scrollIntoView({
+//     behavior: 'smooth'
+//   });
+
+//   document.querySelectorAll('a[href="#about"]')[0].addEventListener('click', (event) => {
+//     event.preventDefault();
+//     document.querySelector('#about').scrollIntoView({
+//       behavior: 'smooth'
+//     });
+//   });
+
+// });
+
+window.onload = function() {
+  var content = document.getElementById('content')
+  content.style.opacity = 1;
+  content.style.display = 'inherit';
+  content.scrollIntoView({ behavior: 'smooth' });
+}
